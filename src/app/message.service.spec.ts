9f65b7ff61ca4e7d8f8e7ca8a4f6477128229cbe
pranayreddy03 <reddy.pranay03@gmail.com>
2018-11-18 12:51:28 -0600

    Unit tested messages service

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -2,7 +2,34 @@ import {MessageService} from './message.service';
 
 describe('Message Service', () => {
 
-const message = new MediaDeviceInfo();
+const service: MessageService;
 
-it('Should be ')
+beforeEach(() => {
+
+})
+
+  it('Should have no messages to start with', () => {
+    service = new MessageService();
+
+    expect(service.messages.length).toBe(0);
+  });
+
+  it('Should add a message when add is called', () => {
+    service = new MessageService();
+    service.add('message1');
+
+    expect(service.messages.length).toBe(1);
+  });
+
+  it('Should clear the messages array when clear function is called', () => {
+    // Arrange
+    service = new MessageService();
+    service.add('message1');
+
+    // Act
+    service.clear();
+
+    // Assert
+    expect(service.messages.length).toBe(0);
+  });
 });
